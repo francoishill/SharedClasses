27f788f92ec2703bb72199f7c98de5baaa04aa76
francoishill <francoishill@3f1363cb-6aae-0410-a37d-b56c5251702a>
2011-10-06 06:52:18 +0000

    UserMessages added (warning, etc).
    git-svn-id: svn://localhost/sharedclasses/trunk@8 3f1363cb-6aae-0410-a37d-b56c5251702a

--- UserMessages.cs
+++ UserMessages.cs
@@ -1,13 +1,33 @@
 using System.Windows.Forms;
 public class UserMessages
 {
-	public static void ShowErrorMessage(string Message, string Title = "Error")
+	private static void ShowMsg(IWin32Window Owner, string Message, string Title, MessageBoxIcon icon)
 	{
-		MessageBox.Show(Message, Title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+		MessageBox.Show(Owner, Message, Title, MessageBoxButtons.OK, icon);
 	}
 
-	public static bool Confirm(string Message, string Title = "Confirm", bool DefaultYesButton = false)
+	public static void ShowErrorMessage(string Message, string Title = "Error", IWin32Window owner = null)
 	{
-		return MessageBox.Show(Message, Title, MessageBoxButtons.YesNo, MessageBoxIcon.Question, DefaultYesButton ? MessageBoxDefaultButton.Button1 : MessageBoxDefaultButton.Button2) == DialogResult.Yes;
+		ShowMsg(owner, Message, Title, MessageBoxIcon.Error);
+	}
+
+	public static void ShowWarningMessage(string Message, string Title = "Warning", IWin32Window owner = null)
+	{
+		ShowMsg(owner, Message, Title, MessageBoxIcon.Warning);
+	}
+
+	public static void ShowInfoMessage(string Message, string Title = "Warning", IWin32Window owner = null)
+	{
+		ShowMsg(owner, Message, Title, MessageBoxIcon.Information);
+	}
+
+	public static void ShowMessage(string Message, string Title = "Warning", IWin32Window owner = null)
+	{
+		ShowMsg(owner, Message, Title, MessageBoxIcon.None);
+	}
+
+	public static bool Confirm(string Message, string Title = "Confirm", bool DefaultYesButton = false, IWin32Window owner = null)
+	{
+		return MessageBox.Show(owner, Message, Title, MessageBoxButtons.YesNo, MessageBoxIcon.Question, DefaultYesButton ? MessageBoxDefaultButton.Button1 : MessageBoxDefaultButton.Button2) == DialogResult.Yes;
 	}
 }

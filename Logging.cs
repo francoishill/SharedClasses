20360c00361f9957f36a4368b3e5c4c535b415b5
francoishill <francoishill@3f1363cb-6aae-0410-a37d-b56c5251702a>
2011-10-09 18:25:59 +0000

    git-svn-id: svn://localhost/sharedclasses/trunk@17 3f1363cb-6aae-0410-a37d-b56c5251702a

--- Logging.cs
+++ Logging.cs
@@ -6,12 +6,17 @@ using System.Windows.Forms;
 
 public class Logging
 {
+	public static NotifyIcon staticNotifyIcon = null;
+
 	public static void appendLogTextbox_OfPassedTextbox(TextBox messagesTextbox, string str, NotifyIcon notifyicon = null)
 	{
+		NotifyIcon notifyIconToUse = null;
+		if (notifyicon != null) notifyIconToUse = notifyicon;
+		else if (staticNotifyIcon != null) notifyIconToUse = staticNotifyIcon;
 		//label1.Text = str;
 		Control topParent = messagesTextbox;
 		while (topParent != null && topParent != messagesTextbox.Parent) topParent = messagesTextbox.Parent;
-		if (notifyicon != null) notifyicon.ShowBalloonTip(3000, "Message", str, ToolTipIcon.Info);
+		if (staticNotifyIcon != null) staticNotifyIcon.ShowBalloonTip(3000, "Message in textbox", str, ToolTipIcon.Info);
 		ThreadingInterop.UpdateGuiFromThread(messagesTextbox, () =>
 		{
 			messagesTextbox.Text = str + (messagesTextbox.Text.Length > 0 ? Environment.NewLine : "") + messagesTextbox.Text;

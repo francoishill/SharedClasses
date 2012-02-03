1db132c2f11b7bf570d20f4251c1483df90f3be4
francoishill <francoishill@3f1363cb-6aae-0410-a37d-b56c5251702a>
2012-02-03 22:34:16 +0000

    git-svn-id: svn://localhost/sharedclasses/trunk@243 3f1363cb-6aae-0410-a37d-b56c5251702a

--- ThreadingInterop.cs
+++ ThreadingInterop.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
 
 public class ThreadingInterop
@@ -70,6 +72,106 @@ public class ThreadingInterop
 		else
 		{
 			txtBox.AutoCompleteCustomSource.Add(textToAdd);
+		}
+	}
+
+	/// <summary>
+	/// Usage: using (new WaitIndicator()) { Your code here... }
+	/// </summary>
+	public class WaitIndicator : IDisposable
+	{
+		class ProgressForm : Form
+		{
+			public ProgressForm()
+			{
+				ControlBox = false;
+				ShowInTaskbar = false;
+				StartPosition = FormStartPosition.CenterScreen;
+				TopMost = true;
+				FormBorderStyle = FormBorderStyle.None;
+				var progreassBar = new ProgressBar()
+				{
+					Style = ProgressBarStyle.Marquee,
+					Size = new System.Drawing.Size(200, 20),
+					Value = 40,
+					ForeColor = Color.Orange,
+					BackColor = Color.Purple,
+					MarqueeAnimationSpeed = 40
+				};
+				AutoSize = true;
+				AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+				//Size = progreassBar.Size;
+				Controls.Add(progreassBar);
+
+				progreassBar.MouseDown += (snder, evtargs) =>
+				{
+					firstPoint = evtargs.Location;
+					IsMouseDown = true;
+				};
+				progreassBar.MouseUp += (snder, evtargs) =>
+				{
+					IsMouseDown = false;
+				};
+				progreassBar.MouseMove += (snder, evtargs) =>
+				{
+					this.OnMouseMove(evtargs);
+				};
+			}
+
+			private bool IsMouseDown = false;
+			private Point firstPoint;
+			protected override void OnMouseDown(MouseEventArgs e)
+			{
+				firstPoint = e.Location;
+				IsMouseDown = true;
+				base.OnMouseDown(e);
+			}
+
+			protected override void OnMouseUp(MouseEventArgs e)
+			{
+				IsMouseDown = false;
+				base.OnMouseUp(e);
+			}
+
+			protected override void OnMouseMove(MouseEventArgs e)
+			{
+				if (IsMouseDown)
+				{
+					// Get the difference between the two points
+					int xDiff = firstPoint.X - e.Location.X;
+					int yDiff = firstPoint.Y - e.Location.Y;
+
+					// Set the new point
+					int x = this.Location.X - xDiff;
+					int y = this.Location.Y - yDiff;
+					this.Location = new Point(x, y);
+				} base.OnMouseMove(e);
+			}
+		}
+
+		ProgressForm progressForm;
+		Thread thread;
+		bool disposed = false; //to avoid redundant call
+		public WaitIndicator()
+		{
+			progressForm = new ProgressForm();
+			thread = new Thread(_ => progressForm.ShowDialog());
+			thread.Start();
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				thread.Abort();
+				progressForm = null;
+			}
+			disposed = true;
 		}
 	}
 }

869bd76e00e59b189abb8d8e3185495e0c38f8ef
francoishill <francoishill@3f1363cb-6aae-0410-a37d-b56c5251702a>
2012-05-14 06:11:48 +0000

    refs #1: Added DropboxInterop.
    git-svn-id: svn://localhost/sharedclasses/trunk@307 3f1363cb-6aae-0410-a37d-b56c5251702a

---
+++ DropboxInterop.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DropNet;
+using System.Windows.Forms;
+
+namespace SharedClasses
+{
+	public class DropboxInterop
+	{
+		public class FranDropboxClient : DropNetClient
+		{
+
+			public bool SuccessfullyAuthenticated = false;
+			private WebBrowser webBrowser1;
+
+			public FranDropboxClient(string apiKey, string appSecret)
+				: base(apiKey, appSecret)
+			{
+				this.webBrowser1 = new WebBrowser() { ScriptErrorsSuppressed = true };
+				this.UseSandbox = true;
+				this.GetTokenAsync(userToken =>
+				{
+					var tokenUrl = this.BuildAuthorizeUrl("http://dkdevelopment.net/BoxShotLogin.htm");
+					webBrowser1.Navigated += new WebBrowserNavigatedEventHandler(webBrowser1_Navigated);
+					if (webBrowser1.InvokeRequired)
+						webBrowser1.Invoke((Action)delegate { webBrowser1.Navigate(new Uri(tokenUrl)); });
+					else
+						webBrowser1.Navigate(new Uri(tokenUrl));
+				},
+				(error) =>
+				{
+					MessageBox.Show(error.Message);
+				});
+			}
+
+			private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+			{
+				if (e.Url.AbsolutePath == "/BoxShotLogin.htm")
+				{
+					this.GetAccessTokenAsync(response =>
+					{
+						this.SuccessfullyAuthenticated = true;
+					},
+					(error) =>
+					{
+						MessageBox.Show("Error with authentication: " + error.Message);
+					});
+				}
+				else
+				{
+					HtmlElement head = webBrowser1.Document.GetElementsByTagName("head")[0];
+					HtmlElement scriptEl = webBrowser1.Document.CreateElement("script");
+					dynamic element = scriptEl.DomElement;
+					element.text = "function clickAllow() { document.getElementsByName('allow_access')[0].click(); }";
+					head.AppendChild(scriptEl);
+					webBrowser1.Document.InvokeScript("clickAllow");
+				}
+			}
+		}
+	}
+}
--- ThreadingInterop.cs
+++ ThreadingInterop.cs
@@ -10,7 +10,7 @@ public class ThreadingInterop
 	private static bool AlreadyAttachedToApplicationExitEvent = false;
 	//TODO: Have a look at this function (automatically queues to a thread) - System.Threading.ThreadPool.QueueUserWorkItem()
 	//PerformVoidFunctionSeperateThread(() => { MessageBox.Show("Test"); MessageBox.Show("Test1"); });
-	public static void PerformVoidFunctionSeperateThread(MethodInvoker method, bool WaitUntilFinish = true, string ThreadName = "UnknownName", bool CheckInvokeRequired = false, Control controlToCheckInvokeRequired = null)
+	public static Thread PerformVoidFunctionSeperateThread(MethodInvoker method, bool WaitUntilFinish = true, string ThreadName = "UnknownName", bool CheckInvokeRequired = false, Control controlToCheckInvokeRequired = null)
 	{
 		if (!AlreadyAttachedToApplicationExitEvent)
 		{
@@ -37,12 +37,18 @@ public class ThreadingInterop
 		th.Start();
 		//th.Join();
 		if (WaitUntilFinish)
+		{
 			while (th.IsAlive && !ForceExitAllTreads) { Application.DoEvents(); }
+			//th.Join();Cannot use this, makes QuickAccess not work (window does not want to show when clicking on tray icon)
+			th.Abort();
+			th = null;
+			return null;
+		}
 		else
+		{
 			Application.DoEvents();
-		//th.Join();Cannot use this, makes QuickAccess not work (window does not want to show when clicking on tray icon)
-		th.Abort();
-		th = null;
+			return th;
+		}
 	}
 
 	public static void UpdateGuiFromThread(Control controlToUpdate, Action action)

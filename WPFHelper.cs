4f7f2399d856774b1620e65ab47834141d7d6de7
Francois Hill <francoishill11@gmail.com>
2013-07-23 07:45:04 +0200

    Added the method 'HideMinimizeAndMaximizeButtons' for WPF windows.

--- WPFHelper.cs
+++ WPFHelper.cs
@@ -82,6 +82,25 @@ namespace SharedClasses
 
 		public static IntPtr GetWindowHandle(this Window window) { return new WindowInteropHelper(window).Handle; }
 
+		// from winuser.h
+		private const int GWL_STYLE      = -16,
+                      WS_MAXIMIZEBOX = 0x10000,
+                      WS_MINIMIZEBOX = 0x20000;
+
+		[DllImport("user32.dll")]
+		extern private static int GetWindowLong(IntPtr hwnd, int index);
+
+		[DllImport("user32.dll")]
+		extern private static int SetWindowLong(IntPtr hwnd, int index, int value);
+
+		public static void HideMinimizeAndMaximizeButtons(this Window window)
+		{
+			IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(window).Handle;
+			var currentStyle = GetWindowLong(hwnd, GWL_STYLE);
+
+			SetWindowLong(hwnd, GWL_STYLE, (currentStyle & ~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX));
+		}
+
 		private static string GetLastWindowPositionFilename(string applicationName, string subfolderName = null)
 		{
 			return SettingsInterop.GetFullFilePathInLocalAppdata("LastWindowPos.fjset", applicationName, subfolderName);

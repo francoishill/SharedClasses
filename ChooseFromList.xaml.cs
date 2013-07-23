19b7f3b2ca6c8b9ecf4e96e9a61ac915d1da1093
Francois Hill <francoishill11@gmail.com>
2013-07-23 09:15:46 +0200

    Added the ChooseFromList.xaml window. Just takes in a list of items and the user must choose or type text.

---
+++ ChooseFromList.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace SharedClasses
+{
+	/// <summary>
+	/// Interaction logic for ChooseFromList.xaml
+	/// </summary>
+	public partial class ChooseFromList : Window
+	{
+		string NameOfAnItem;
+
+		public ChooseFromList(string nameOfAnItem, IEnumerable<object> listOfItems)
+		{
+			InitializeComponent();
+			this.NameOfAnItem = nameOfAnItem;
+			this.labelUserMessage.Content = "Please select (or type) a " + nameOfAnItem;
+			this.comboBoxList.ItemsSource = listOfItems;
+		}
+
+		private void buttonCancel_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = false;
+			this.Close();
+		}
+
+		private void buttonOK_Click(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(comboBoxList.Text))
+			{
+				UserMessages.ShowWarningMessage(string.Format("Please enter (or choose) a valid {0}.", this.NameOfAnItem));
+				comboBoxList.Focus();
+				return;
+			}
+
+			this.DialogResult = true;
+			this.Close();
+		}
+	}
+}
